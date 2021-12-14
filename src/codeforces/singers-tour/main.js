92db6edba79ac69de12343273a039543d94c28bf
Bernardo <bernardofbbraga@gmail.com>
2021-12-14 18:56:27 -0300

    singers-tour com rounding error

---
+++ src/codeforces/singers-tour/main.js
@@ -0,0 +1,88 @@
+"use strict";
+
+function matprint(mat) {
+  let shape = [mat.length, mat[0].length];
+  function col(mat, i) {
+    return mat.map((row) => row[i]);
+  }
+  let colMaxes = [];
+  for (let i = 0; i < shape[1]; i++) {
+    colMaxes.push(
+      Math.max.apply(
+        null,
+        col(mat, i).map((n) => n.toString().length)
+      )
+    );
+  }
+
+  mat.forEach((row) => {
+    console.log.apply(
+      null,
+      row.map((val, j) => {
+        return new Array(colMaxes[j] - val.toString().length + 1).join(" ") + val.toString() + "  ";
+      })
+    );
+  });
+}
+
+const nQueues = this.readline().split(" ");
+
+for (let i = 0; i < nQueues; i++) {
+  const n = Number(this.readline());
+  const minutes = this.readline()
+    .split(" ")
+    .map((e) => Number(e));
+  console.log(n, minutes);
+
+  // Prepare matrix with the coeficient
+  const matrix = [];
+  for (let city = 0; city < n; city++) {
+    matrix[city] = [];
+    for (let singer = 0; singer < n; singer++) {
+      /*
+        1 3 2
+        2 1 3
+        3 2 1
+      */
+      matrix[city][singer] = 1 + ((city + (n-singer)) % n);
+    }
+    matrix[city].push(minutes[city]);
+  }
+
+  matprint(matrix);
+
+  for (let a = 0; a < n; a++) {
+    const f1 = matrix[a][a];
+    for (let col = a; col <= n; col++) {
+      matrix[a][col] = matrix[a][col] / f1;
+    }
+    for (let lin = a + 1; lin < n; lin++) {
+      const f2 = matrix[lin][a];
+      for (let col = a; col <= n; col++) {
+        matrix[lin][col] = matrix[lin][col] - f2 * matrix[a][col];
+      }
+    }
+  }
+  
+  for (let a = n - 1; a >= 0; a--) {
+    for (let lin = a - 1; lin >= 0; lin--) {
+      const fator = matrix[lin][a];
+      for (let col = n; col >= a; col--) {
+        matrix[lin][col] = matrix[lin][col] - fator * matrix[a][col];
+      }
+    }
+  }
+
+  const vars = [];
+  for (let a = 0; a < n; a++) {
+    if (matrix[a][n] <= 0) {
+      this.print("NO");
+      break;
+    }
+    vars.push(matrix[a][n]);
+  }
+  if (vars.length === n) {
+    this.print("YES");
+    this.print(vars.join(" "));
+  }
+}
