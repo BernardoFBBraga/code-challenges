6be19b3cdf1afb625a13dfa90835f3f14956a73d
Bernardo <bernardofbbraga@gmail.com>
2021-12-14 12:19:48 -0300

    cleanup

--- src/codeforces/polycarp/main.js
+++ src/codeforces/polycarp/main.js
@@ -1,10 +1,5 @@
 "use strict";
 
-function answer(s) {
-  if (s.length > 10) return s[0] + (s.length - 2) + s[s.length - 1];
-  return s;
-}
-
 const nQueues = this.readline().split(" ");
 
 for (let i = 0; i < nQueues; i++) {
