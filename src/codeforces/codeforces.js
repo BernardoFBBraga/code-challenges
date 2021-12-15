72fe0182b71e23c3603c470372903d3956f2dc65
Bernardo <bernardofbbraga@gmail.com>
2021-12-15 09:34:30 -0300

    improve script to accept single test cases

--- src/codeforces/codeforces.js
+++ src/codeforces/codeforces.js
@@ -1,5 +1,7 @@
 "use strict";
 const myArgs = process.argv.slice(2);
+const problemName = myArgs[0];
+const testCase = myArgs[1];
 
 const fs = require("fs");
 
@@ -9,38 +11,45 @@ process.stdin.setEncoding("utf-8");
 async function ls(path) {
   const dir = await fs.promises.opendir(path);
   for await (const dirent of dir) {
-    console.log(dirent.name);
-    const inputString = fs.readFileSync(`./${myArgs[0]}/inputs/${dirent.name}`).toString().split("\n");
-    let currentLine = 0;
-    function readLine() {
-      return inputString[currentLine++];
-    }
+    if (!testCase || dirent.name.includes(testCase)) {
+      console.log(dirent.name);
+      const inputString = fs.readFileSync(`./${problemName}/inputs/${dirent.name}`).toString().split("\n");
+      let currentLine = 0;
+      function readLine() {
+        return inputString[currentLine++];
+      }
 
-    let printString = "";
-    function print(s) {
-      printString += s;
-      printString += "\n";
-    }
+      let printString = "";
+      function print(s) {
+        printString += s;
+        printString += "\n";
+      }
 
-    function main() {
-      const code = fs.readFileSync(`./${myArgs[0]}/main.js`).toString();
-      eval(code);
-    }
+      function main() {
+        const code = fs.readFileSync(`./${problemName}/main.js`).toString();
+        eval(code);
+      }
 
-    main.call({ readline: readLine, print: print });
+      main.call({ readline: readLine, print: print });
 
-    const expectedOutput = fs.readFileSync(`./${myArgs[0]}/outputs/${dirent.name}`).toString();
+      const expectedOutput = fs.readFileSync(`./${problemName}/outputs/${dirent.name}`).toString();
 
-    if (printString === expectedOutput) console.log("\x1b[42m\x1b[30mPassed\x1b[0m");
-    else {
-      console.log("\x1b[41m\x1b[30mFailed\x1b[0m");
-      console.log("Printed:");
-      console.log(printString);
-      console.log("Expected:");
-      console.log(expectedOutput);
+      if (printString === expectedOutput) console.log("\x1b[42m\x1b[30mPassed\x1b[0m");
+      else {
+        console.log("\x1b[41m\x1b[30mFailed\x1b[0m");
+        console.log("Printed:");
+        console.log(printString);
+        console.log("Expected:");
+        console.log(expectedOutput);
+      }
     }
   }
   process.exit();
 }
 
-ls(`./${myArgs[0]}/inputs`).catch(console.error);
+if (problemName) {
+  ls(`./${problemName}/inputs`).catch(console.error);
+} else {
+  console.log('Usage: node codeforces folderName [partialTestCaseName]')
+  process.exit()
+}
